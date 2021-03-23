080a9adaacefd4391c40f39cbaff7a264222f6c6
QuanDao <quanbn1336@gmail.com>
2021-03-23 17:57:47 +0700

    update code

--- src/search/search_feature.py
+++ src/search/search_feature.py
@@ -1,9 +1,12 @@
 import os
 import sys
+import elasticsearch as es
 
 
 class _Search(object):
-
-
+    def __init__(self):
+        pass
+    def _index(self):
+        pass
     def search_feature(self):
         pass
