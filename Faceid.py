e949d0db198416c3fd330ab558b1b37062317da4
QuanDao <quanbn1336@gmail.com>
2021-03-23 12:57:23 +0700

    app

--- Faceid.py
+++ Faceid.py
@@ -1,13 +1,22 @@
+from __future__ import unicode_literals
+#from src.BaseModel.model_Face import
+from src.BaseModel.detector import FACE_DETECT
 import configparser
 import logging
+import cv2
+import os
+
 
 logging.basicConfig()
+
+
 class Face_id(object):
     def __init__(self):
         cfg = configparser.ConfigParser()
         cfg.read('config.ini')
         camera = cfg.get('Default', 'camera_url')
         model = cfg.get('Model', 'model_url')
+
     def convert_image(self):
         pass
 
@@ -16,7 +25,3 @@ class Face_id(object):
 
     def run(self):
         pass
-
-
-
-
