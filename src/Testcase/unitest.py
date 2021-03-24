afe0719c9b602738be69bc5667f50b8f75db6b36
QuanDao <quanbn1336@gmail.com>
2021-03-24 13:51:36 +0700

    update

---
+++ src/Testcase/unitest.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+import unitest
+import sys
+import os
+
+
+class TestCase(unitest):
+    def __init__(self):
+        pass
+
+    def test(self):
+        pass
