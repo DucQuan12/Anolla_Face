6e085baf4e911a66dc52d308368d41be6c176932
QuanDao <quanbn1336@gmail.com>
2021-03-30 14:44:17 +0700

    modify

---
+++ src/src/BaseModel/FaceDetect/FastMTCNN.py
@@ -0,0 +1,15 @@
+# from __future__ import absolute_import
+#
+# from facenet_pytorch import MTCNN
+# import torch
+# import logging
+# import cv2
+# from imutils.video import FileVideoStream
+# import time
+# import os
+# import os
+#
+# logging.basicConfig()
+#
+# class FastMTCNN(object):
+#
--- src/utils/license.py
+++ src/utils/license.py
@@ -1,1 +1,3 @@
 import Licensing
+import AppConfig
+
