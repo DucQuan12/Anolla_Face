7e632c7cbb2656df3f1629d0a06acfbd43d87bf3
Luong Hung Son <luonghungewsxz@gmail.com>
2021-04-07 10:08:16 +0700

    get embedding function

--- src/src/processor.py
+++ src/src/processor.py
@@ -1,11 +1,18 @@
-from Face import VGGFace2
+from Face.VGGFace2 import ModelFace
+from keras_vggface.utils import preprocess_input
+import numpy as np
+import cv2
 
 
 class ModelProcessor(object):
     def __init__(self):
-        pass
-
+        self.vgg_model = ModelFace.load_model()
+        
     def predict(self, image):
-        # 1 list embeedef
-        pass
+        img = cv2.resize(image, (224, 224))
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        samples = np.expand_dims(img, axis=0)
+        samples = preprocess_input(samples, version=2)
+        embeddings = self.vgg_model.predict(samples)
+        return embeddings
 
